8387a72d841f11c42d688ab22a2743b504b17f72
Wiktor <74769437+Wikierk@users.noreply.github.com>
2024-05-30 21:36:04 +0200

    added color picker

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -8,7 +8,7 @@
 #include <QPainter>
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), image(800, 600, QImage::Format_RGB32)
+    : QMainWindow(parent), ui(new Ui::MainWindow), image(800, 600, QImage::Format_RGB32), selectedColorBuddhabrot(Qt::white), selectedColorNewton(Qt::red)
 {
     ui->setupUi(this);
     ui->iterationsCountlabel->setText(QString::number(ui->iterationsNewtonSlider->value()));
@@ -24,130 +24,134 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushNewtonBtn_clicked()
 {
     generateNewtonFractal(image, image.width(), image.height(), 0, 0,
-        ui->iterationsNewtonSlider->value());
+        ui->iterationsNewtonSlider->value(), selectedColorNewton);
     ui->labelView->setPixmap(QPixmap::fromImage(image));
 }
 
 void MainWindow::on_pushBuddhaBtn_clicked()
 {
     generateBuddhabrotFractal(image, image.width(), image.height(), 0, 0,
-        ui->buddhabrotIterationsSlider->value(), ui->buddhabrotSamplesSlider->value());
+        ui->buddhabrotIterationsSlider->value(), ui->buddhabrotSamplesSlider->value(), selectedColorBuddhabrot);
     ui->labelView->setPixmap(QPixmap::fromImage(image));
 }
 
 void MainWindow::on_pushCompareBtn_clicked()
 {
     generateNewtonFractal(image, image.width(), image.height() / 2, 0, 0,
-        ui->iterationsNewtonSlider->value());
+        ui->iterationsNewtonSlider->value(), selectedColorNewton);
     generateBuddhabrotFractal(image, image.width(), image.height() / 2, 0, image.height() / 2,
-        ui->iterationsNewtonSlider->value(), ui->buddhabrotSamplesSlider->value());
+        ui->iterationsNewtonSlider->value(), ui->buddhabrotSamplesSlider->value(), selectedColorBuddhabrot);
     ui->labelView->setPixmap(QPixmap::fromImage(image));
 }
 
 void MainWindow::generateNewtonFractal(QImage& image, int width, int height, int xOffset,
-    int yOffset, int maxIterations)
+    int yOffset, int maxIterations, const QColor& color)
 {
     double scale = 3.0;
-    double offsetX = -1.5;
-    double offsetY = -1.5;
+      double offsetX = -1.5;
+      double offsetY = -1.5;
 
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            std::complex<double> z(scale * (x - width / 2.0) / (width / 2.0) + offsetX,
-                scale * (y - height / 2.0) / (height / 2.0) + offsetY);
+      for (int y = 0; y < height; ++y) {
+          for (int x = 0; x < width; ++x) {
+              std::complex<double> z(scale * (x - width / 2.0) / (width / 2.0) + offsetX,
+                                     scale * (y - height / 2.0) / (height / 2.0) + offsetY);
 
-            int iteration = 0;
-            const std::complex<double> roots[] = { std::complex<double>(1, 0),
-                std::complex<double>(-0.5, sqrt(3) / 2), std::complex<double>(-0.5, -sqrt(3) / 2) };
-            const QColor colors[] = { Qt::red, Qt::green, Qt::blue };
+              int iteration = 0;
+              const std::complex<double> roots[] = { std::complex<double>(1, 0),
+                  std::complex<double>(-0.5, sqrt(3) / 2), std::complex<double>(-0.5, -sqrt(3) / 2) };
+              const QColor colors[] = { color, Qt::green, Qt::blue }; // Użycie wybranego koloru
 
-            while (iteration < maxIterations) {
-                std::complex<double> f = z * z * z - 1.0;
-                std::complex<double> f_prime = 3.0 * z * z;
-                std::complex<double> z_next = z - f / f_prime;
+              while (iteration < maxIterations) {
+                  std::complex<double> f = z * z * z - 1.0;
+                  std::complex<double> f_prime = 3.0 * z * z;
+                  std::complex<double> z_next = z - f / f_prime;
 
-                if (std::abs(z_next - z) < 1e-6)
-                    break;
+                  if (std::abs(z_next - z) < 1e-6)
+                      break;
 
-                z = z_next;
-                ++iteration;
-            }
+                  z = z_next;
+                  ++iteration;
+              }
 
-            QColor color = Qt::black;
+              QColor pixelColor = Qt::black;
 
-            if (iteration < maxIterations) {
-                double min_distance = std::numeric_limits<double>::max();
-                int root_index = -1;
+              if (iteration < maxIterations) {
+                  double min_distance = std::numeric_limits<double>::max();
+                  int root_index = -1;
 
-                for (int i = 0; i < 3; ++i) {
-                    double distance = std::abs(z - roots[i]);
-                    if (distance < min_distance) {
-                        min_distance = distance;
-                        root_index = i;
-                    }
-                }
+                  for (int i = 0; i < 3; ++i) {
+                      double distance = std::abs(z - roots[i]);
+                      if (distance < min_distance) {
+                          min_distance = distance;
+                          root_index = i;
+                      }
+                  }
 
-                if (root_index != -1)
-                    color = colors[root_index];
-            }
+                  if (root_index != -1)
+                      pixelColor = colors[root_index];
+              }
 
-            image.setPixel(x + xOffset, y + yOffset, color.rgb());
-        }
-    }
+              image.setPixel(x + xOffset, y + yOffset, pixelColor.rgb());
+          }
+      }
 }
 
 void MainWindow::generateBuddhabrotFractal(QImage& image, int width, int height, int xOffset,
-    int yOffset, int maxIterations, int numSamples)
+    int yOffset, int maxIterations, int numSamples, const QColor& color)
 {
 
     std::vector<int> histogram(width * height, 0);
 
-    auto scale = [=](double value, double minSrc, double maxSrc, double minDst, double maxDst) {
-        return minDst + (value - minSrc) * (maxDst - minDst) / (maxSrc - minSrc);
-    };
+       auto scale = [=](double value, double minSrc, double maxSrc, double minDst, double maxDst) {
+           return minDst + (value - minSrc) * (maxDst - minDst) / (maxSrc - minSrc);
+       };
 
-    std::vector<std::complex<double>> trajectory;
+       std::vector<std::complex<double>> trajectory;
 
-    for (int i = 0; i < numSamples; ++i) {
-        double cr = scale(rand() / (double)RAND_MAX, 0, 1, -2, 2);
-        double ci = scale(rand() / (double)RAND_MAX, 0, 1, -2, 2);
-        std::complex<double> c(cr, ci);
-        std::complex<double> z = c;
+       for (int i = 0; i < numSamples; ++i) {
+           double cr = scale(rand() / (double)RAND_MAX, 0, 1, -2, 2);
+           double ci = scale(rand() / (double)RAND_MAX, 0, 1, -2, 2);
+           std::complex<double> c(cr, ci);
+           std::complex<double> z = c;
 
-        int iteration;
+           int iteration;
 
-        for (iteration = 0; iteration < maxIterations; ++iteration) {
-            if (std::norm(z) > 4.0) {
-                break;
-            }
-            z = z * z + c;
-            trajectory.push_back(z);
-        }
+           for (iteration = 0; iteration < maxIterations; ++iteration) {
+               if (std::norm(z) > 4.0) {
+                   break;
+               }
+               z = z * z + c;
+               trajectory.push_back(z);
+           }
 
-        if (iteration < maxIterations) {
-            for (const auto& point : trajectory) {
-                int x = scale(point.real(), -2, 2, 0, width);
-                int y = scale(point.imag(), -2, 2, 0, height);
+           if (iteration < maxIterations) {
+               for (const auto& point : trajectory) {
+                   int x = scale(point.real(), -2, 2, 0, width);
+                   int y = scale(point.imag(), -2, 2, 0, height);
 
-                if (x >= 0 && x < width && y >= 0 && y < height) {
-                    ++histogram[y * width + x];
-                }
-            }
-        }
+                   if (x >= 0 && x < width && y >= 0 && y < height) {
+                       ++histogram[y * width + x];
+                   }
+               }
+           }
 
-        trajectory.clear();
-    }
+           trajectory.clear();
+       }
 
-    int maxCount = *std::max_element(histogram.begin(), histogram.end());
+       int maxCount = *std::max_element(histogram.begin(), histogram.end());
 
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            int count = histogram[y * width + x];
-            int colorValue = scale(std::log(count + 1), 0, std::log(maxCount + 1), 0, 255);
-            image.setPixel(x + xOffset, y + yOffset,
-                QColor(colorValue, colorValue, colorValue).rgb());
-        }
-    }
+       for (int y = 0; y < height; ++y) {
+           for (int x = 0; x < width; ++x) {
+               int count = histogram[y * width + x];
+               int colorValue = scale(std::log(count + 1), 0, std::log(maxCount + 1), 0, 255);
+
+               int red = scale(colorValue, 0, 255, 0, color.red());
+               int green = scale(colorValue, 0, 255, 0, color.green());
+               int blue = scale(colorValue, 0, 255, 0, color.blue());
+
+               image.setPixel(x + xOffset, y + yOffset, QColor(red, green, blue).rgb());
+           }
+       }
 }
 
 
@@ -169,3 +173,21 @@ void MainWindow::on_buddhabrotSamplesSlider_valueChanged(int value)
     ui->sampleCountLabel->setText(QString::number(value));
 }
 
+
+void MainWindow::on_colorPickerBuddhabrot_clicked()
+{
+    QColor color = QColorDialog::getColor(Qt::white, this, "Wybierz kolor");
+
+        if (color.isValid()) {
+            selectedColorBuddhabrot = color;
+        }
+}
+
+void MainWindow::on_colorPickerNewton_clicked()
+{
+    QColor color = QColorDialog::getColor(Qt::white, this, "Wybierz kolor");
+
+        if (color.isValid()) {
+            selectedColorNewton = color;
+        }
+}
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -3,6 +3,7 @@
 
 #include <QLabel>
 #include <QMainWindow>
+#include <QColorDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,22 +23,24 @@ private slots:
     void on_pushNewtonBtn_clicked();
     void on_pushBuddhaBtn_clicked();
     void on_pushCompareBtn_clicked();
+    void on_colorPickerBuddhabrot_clicked();
 
     void on_iterationsNewtonSlider_valueChanged(int value);
-
     void on_buddhabrotIterationsSlider_valueChanged(int value);
-
     void on_buddhabrotSamplesSlider_valueChanged(int value);
 
+    void on_colorPickerNewton_clicked();
 
 private:
     void generateNewtonFractal(QImage& image, int width, int height, int xOffset, int yOffset,
-        int maxIterations);
+        int maxIterations, const QColor& color);
     void generateBuddhabrotFractal(QImage& image, int width, int height, int xOffset, int yOffset,
-        int maxIterations, int numSamples);
+        int maxIterations, int numSamples, const QColor& color);
 
 private:
     Ui::MainWindow* ui;
     QImage image;
+    QColor selectedColorBuddhabrot;
+    QColor selectedColorNewton;
 };
 #endif // MAINWINDOW_H
